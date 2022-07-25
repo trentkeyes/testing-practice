71c27ef28848db6ad41f19732e35702404b4f0fe
Trent Keyes <tkeyes@gmail.com>
2022-07-25 17:34:19 -0500

    Finish calculator and caesers cipher problems

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,3 +1,28 @@
-const caesarCipher = () => {};
+const caesarCipher = (str) => {
+  let encrypted = '';
+  for (const letter of str) {
+    if (letter === ' ') {
+      encrypted += ' ';
+    } else {
+      const charCode = letter.charCodeAt();
+      if (charCode >= 97 && charCode <= 122) {
+        if (charCode + 13 > 122) {
+          encrypted += String.fromCharCode(charCode - 13);
+        } else {
+          encrypted += String.fromCharCode(charCode + 13);
+        }
+      } else if (charCode >= 65 && charCode <= 90) {
+        if (charCode + 13 > 90) {
+          encrypted += String.fromCharCode(charCode - 13);
+        } else {
+          encrypted += String.fromCharCode(charCode + 13);
+        }
+      } else {
+        encrypted += letter;
+      }
+    }
+  }
+  return encrypted;
+};
 
 module.exports = caesarCipher;
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -1,5 +1,23 @@
 const caesarCipher = require('./caesarCipher');
 
-test('', () => {
-  expect(caesarCipher()).toEqual(undefined);
+test('shift letters 13 places', () => {
+  expect(caesarCipher('attack at dawn')).toEqual('nggnpx ng qnja');
+});
+
+test('shift a different phrase letters 13 places', () => {
+  expect(caesarCipher('no one could crack this code')).toEqual(
+    'ab bar pbhyq penpx guvf pbqr'
+  );
+});
+
+test('With upper case letters', () => {
+  expect(caesarCipher('No one could cracK this Code')).toEqual(
+    'Ab bar pbhyq penpX guvf Pbqr'
+  );
+});
+
+test('With non-letters / symbols', () => {
+  expect(caesarCipher('No one! could cracK this Code :)')).toEqual(
+    'Ab bar! pbhyq penpX guvf Pbqr :)'
+  );
 });
